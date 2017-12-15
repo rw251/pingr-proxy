198aefdcddd7ceba454039313db1b458789af091
Richard Williams <richard.williams2@manchester.ac.uk>
2017-12-15 10:09:52 +0000

    Final try

--- proxy.js
+++ proxy.js
@@ -5,22 +5,17 @@ var proxy = httpProxy.createProxyServer();
 var pingrDev = 'pingr-dev.herokuapp.com';
 var pingrBen = 'pingr-ben.herokuapp.com';
 
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-    proxyReq.setHeader('Host', pingrDev);
-    console.log(proxyReq);
-});
-
-app.all("/app1/*", function(req, res) {
-    console.log('redirecting to pingrDev');
-    proxy.web(req, res, {target: `https://${pingrDev}`});
-});
-
-app.all("/app2/*", function(req, res) {
+app.all("/*", function(req, res) {
     console.log('redirecting to pingrBen');
-    proxy.web(req, res, {target: `https://${pingrBen}`});
+    proxy.web(req, res, {target: `https://${pingrBen}`, changeOrigin: true});
 });
 
-app.get('/', (req, res) => {
+app.all("/beta/*", function(req, res) {
+    console.log('redirecting to pingrDev');
+    proxy.web(req, res, {target: `https://${pingrDev}`, changeOrigin: true});
+});
+
+app.get('/alpha/', (req, res) => {
     res.send('ok');
 });
 
